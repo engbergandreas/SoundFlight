cc744b8921c93121cecf41737902ff91d35b29c4
jonan270 <57438307+jonan270@users.noreply.github.com>
2020-09-17 16:46:34 +0200

    added a sound file

--- SoundFlight/Assets/Scripts C#/Soundtest.cs
+++ SoundFlight/Assets/Scripts C#/Soundtest.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Soundtest : MonoBehaviour
 {
     AudioSource background_music;
     public float[] spectrum;
     public GameObject stapel;
     public GameObject[] stapel_array;
+    public GameObject[] band_array;
+
+    const int LENGTH = 1024;
 
     // Start is called before the first frame update
     void Start()
     {
-      int length = 128;
         background_music = GetComponent<AudioSource>();
-        spectrum = new float[length];
-        stapel_array = new GameObject[length];
+        spectrum = new float[LENGTH];
+        stapel_array = new GameObject[LENGTH];
 
         for(int i = 0; i < spectrum.Length; i++) {
           stapel_array[i] = Instantiate(stapel, new Vector3(i*0.1f,0,0),Quaternion.identity);
         }
-
     }
 
     // Update is called once per frame
