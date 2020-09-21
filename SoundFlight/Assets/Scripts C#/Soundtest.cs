c4cac5805d23fe52ee56bda1b830c7a4af32530f
jonan270 <57438307+jonan270@users.noreply.github.com>
2020-09-21 13:30:42 +0200

    Created proper logarithmic frequency bands

--- SoundFlight/Assets/Scripts C#/Soundtest.cs
+++ SoundFlight/Assets/Scripts C#/Soundtest.cs
@@ -11,8 +11,14 @@ public class Soundtest : MonoBehaviour
     public GameObject stapel;
     public GameObject[] stapel_array;
 
-    const int LENGTH = 1024;
-    const int BANDS = 24;
+
+    const int SAMPLE_RATE = 22050;
+    const int LENGTH = 4096; //Amount of samples
+    const int BANDS = 30; //Amount of eq bands
+
+    //The size of 4096 equally sized sample intervall
+    float hzwidth = SAMPLE_RATE / LENGTH;
+    float multiplier = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -20,38 +26,48 @@ public class Soundtest : MonoBehaviour
         background_music = GetComponent<AudioSource>();
         spectrum = new float[LENGTH];
         stapel_array = new GameObject[BANDS];
+        band_spectrum = new float[BANDS];
 
         for(int i = 0; i < BANDS; i++) {
           stapel_array[i] = Instantiate(stapel, new Vector3(i*0.1f,0,0),Quaternion.identity);
         }
     }
 
-    // Update is called once per frame
+    // Update size every frame
     void Update()
     {
+      //Eqbands creates a spectrum of values for the different frequencybands
       MakeBands();
 
-      //What is channel 0???
+      //FFT spectrum, Hanning window
       background_music.GetSpectrumData(spectrum,0,FFTWindow.Hanning);
 
       for(int i = 0; i < band_spectrum.Length; i++){
         Transform transform = stapel_array[i].GetComponent<Transform>();
-        transform.localScale = new Vector3(0.1f, 10*band_spectrum[i], 0.1f);
-        transform.position = new Vector3(i*0.1f, band_spectrum[i]*5, 0);
+        transform.localScale = new Vector3(0.1f, multiplier*band_spectrum[i], 0.1f);
+        transform.position = new Vector3(i*0.1f, band_spectrum[i]*(multiplier/2), 0);
       }
     }
 
+
     void MakeBands()
     {
-      band_spectrum = new float[BANDS];
-      for(int i = 0; i < BANDS; i++)
+      float cur_hz = 20;
+      int counter = 0;
+
+      float maxval = 0;
+
+      for(int i = 0; i < LENGTH; i++)
       {
-        for(int j = 0; j < LENGTH/BANDS; j++)
-        {
-          //Logarithmic scale
-          band_spectrum[i] += spectrum[j + LENGTH/BANDS*i];
-        }
-        band_spectrum[i] /= LENGTH/BANDS;
+          if(hzwidth*i > cur_hz)
+          {
+            band_spectrum[counter] = Mathf.Log10(1+maxval); //sets max value in the range
+            //reset for new range
+            counter++;
+            cur_hz *= Mathf.Pow(2,1/3f);
+            maxval = 0;
+          }
+          maxval = (maxval < spectrum[i]) ? spectrum[i] : maxval; //find max value in band range
       }
     }
 }
